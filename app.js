b1e4bdeb61b9f195c96c1afbe9a1844ba605a716
Gardeken <gardeken931@gmail.com>
2025-05-22 12:35:15 -0400

    configurando paquetes

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const path = require("path");
 
 app.use(express.json());
 app.use("/", express.static(path.resolve("views", "home")));
+app.use(express.static("src"));
 
 module.exports = app;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.html", "./src/**/*.{js,ts,jsx,tsx,html,vue,svelte}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
