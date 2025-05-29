b557415b8c35011ae1eee43fbbe52cb767c245e9
Gardeken <gardeken931@gmail.com>
2025-05-28 22:43:08 -0400

    terminando observaciones

--- views/catalogo/index.js
+++ views/catalogo/index.js
@@ -14,6 +14,8 @@ ham_icon.addEventListener("click", toggleLateral);
 x_icon.addEventListener("click", toggleLateral);
 
 document.addEventListener("DOMContentLoaded", () => {
+  const spinner = document.querySelector("#spinner");
+  spinner.innerHTML = `<span class="loader"></span>`;
   getAllTelas();
 });
 
@@ -22,12 +24,90 @@ async function getAllTelas() {
   const { data } = consulta;
   filtrarPorTelas(data);
   imprimirTelas(data);
+  filtrarPrecio(data);
+  filtrarNombre(data);
+}
+
+function filtrarPorTelas(list) {
+  const span = containerIdTelas.querySelectorAll("span");
+  span.forEach((div) => {
+    if (div.id == "0") {
+      div.addEventListener("click", () => {
+        imprimirTelas(list);
+      });
+    } else if (div.id) {
+      div.addEventListener("click", () => {
+        const newList = list.filter((tela) => tela.type === Number(div.id));
+        imprimirTelas(newList);
+      });
+    }
+  });
+}
+
+function filtrarNombre(list) {
+  const inputName = document.querySelector("#inputName");
+  inputName.addEventListener("input", () => {
+    if (inputName.value != "") {
+      const inputNameValue = inputName.value;
+      const newList = list.filter((tela) => {
+        const { name } = tela;
+        const validarName = name
+          .toLowerCase()
+          .includes(inputNameValue.toLowerCase());
+        return validarName;
+      });
+      console.log(newList);
+      imprimirTelas(newList);
+    } else {
+      imprimirTelas(list);
+    }
+  });
+}
+
+function filtroPrecio(inputLower, inputHigher, list) {
+  const newList = list.filter((tela) => {
+    const meetsMinPrice =
+      inputLower.value === "" ||
+      parseFloat(tela.price) >= Number(inputLower.value);
+    const meetsMaxPrice =
+      inputHigher.value === "" ||
+      parseFloat(tela.price) <= Number(inputHigher.value);
+    return meetsMinPrice & meetsMaxPrice;
+  });
+  return newList;
+}
+
+function filtrarPrecio(list) {
+  const lowPrice = document.querySelector("#lowPrice");
+  const highPrice = document.querySelector("#highPrice");
+  let newList = list;
+  lowPrice.addEventListener("input", () => {
+    if (lowPrice.value != "") {
+      newList = filtroPrecio(lowPrice, highPrice, list);
+      imprimirTelas(newList);
+    } else {
+      newList = filtroPrecio(lowPrice, highPrice, list);
+      imprimirTelas(newList);
+    }
+  });
+
+  highPrice.addEventListener("input", () => {
+    if (highPrice.value != "") {
+      newList = filtroPrecio(lowPrice, highPrice, list);
+      imprimirTelas(newList);
+    } else {
+      newList = filtroPrecio(lowPrice, highPrice, list);
+      imprimirTelas(newList);
+    }
+  });
 }
 
 function imprimirTelas(list) {
+  const spinner = document.querySelector("#spinner");
   containerTelas.innerHTML = "";
+  spinner.innerHTML = "";
   list.forEach((tela) => {
-    const { name, id, photo, Type, price } = tela;
+    const { name, id, photo, type, price } = tela;
     const a = document.createElement("a");
     a.classList.add(
       "w-auto",
@@ -45,21 +125,5 @@ function imprimirTelas(list) {
           </div>
           `;
     containerTelas.appendChild(a);
-  });
-}
-
-function filtrarPorTelas(list) {
-  const span = containerIdTelas.querySelectorAll("span");
-  span.forEach((div) => {
-    if (div.id == "0") {
-      div.addEventListener("click", () => {
-        imprimirTelas(list);
-      });
-    } else if (div.id) {
-      div.addEventListener("click", () => {
-        const newList = list.filter((tela) => tela.type === Number(div.id));
-        imprimirTelas(newList);
-      });
-    }
   });
 }
